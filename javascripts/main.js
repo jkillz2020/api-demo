9314022f4bfac9f65b0e55a3954c8d7da34a7721
jkillz2020 <kilgore.josh@gmail.com>
2016-10-25 21:38:49 -0500

    fully functional

---
+++ javascripts/main.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let apiKeys = {};
+
+let imageList = (searchText) =>{
+  return new Promise((resolve, reject) =>{
+    $.ajax({
+      method: 'GET',
+      url: 'apikeys.json'
+    }).then(response =>{
+      console.log('response', response);
+      apiKeys = response;
+      let authHeader = "Client-ID " + apiKeys.client_id;
+
+
+$.ajax({
+  method: 'GET',
+  headers:{
+    'Authorization': authHeader
+  },
+  url: `https://api.imgur.com/3/gallery/t/${searchText}`
+}).then( (response2)=>{
+  console.log("imgur response", response);
+  resolve(response2.data.items);
+}, (errorResponse2)=>{
+  console.log('imgur fail', errorResponse2);
+  reject(errorResponse2);
+}, (errorResponse)=>{
+      console.log('errorResponse', errorResponse);
+    });
+  });
+});
+};
+
+
+$(document).ready(function(){
+  $("#clicky-button").on('click', ()=>{
+    $("#clicky-button").button("loading");
+    $('#output').html("");
+    let searchy = $('#imgur-search').val();
+    console.log("its working");
+    imageList(searchy).then((dataFromImgur)=>{
+      $("#clicky-button").button("reset");
+    console.log('dataFromImgur', dataFromImgur);
+    dataFromImgur.forEach((image) =>{
+      $('#output').append(`<img src="${image.link}">`);
+    });
+
+  });
+
+  });
+});
